d1ee94eabfde87a9557c2bb22318ad5493eb2c46
Jmackles <124995231+Jmackles@users.noreply.github.com>
2023-09-28 10:05:39 -0400

    ,

--- eventdb.py
+++ eventdb.py
@@ -18,7 +18,7 @@ def add_event():
     color = data['color']
     
     c.execute("INSERT INTO events (day, name, startTime, endTime, description, color) VALUES (?, ?, ?, ?, ?, ?)",
-              (day, name, startTime, endTime, description, color))
+          (day, name, startTime, endTime, description, color))
     
     conn.commit()
     conn.close()
@@ -28,17 +28,53 @@ def add_event():
 def get_events():
     conn = sqlite3.connect('events.db')
     c = conn.cursor()
-    c.execute("SELECT day, name, startTime, endTime, description, color FROM events")
+    c.execute("SELECT id, day, name, startTime, endTime, description, color FROM events")
     events = c.fetchall()
     conn.close()
     return jsonify(events)
+
+@app.route('/update_event', methods=['PUT'])
+def update_event():
+    conn = sqlite3.connect('events.db')
+    c = conn.cursor()
+    data = request.json
+    event_id = data['id']
+    day = data['day']
+    name = data['name']
+    startTime = data['startTime']
+    endTime = data['endTime']
+    description = data['description']
+    color = data['color']
+
+    c.execute("UPDATE events SET day = ?, name = ?, startTime = ?, endTime = ?, description = ?, color = ? WHERE id = ?",
+              (day, name, startTime, endTime, description, color, event_id))
+
+    conn.commit()
+    conn.close()
+    return jsonify({"status": "success"})
+
+
+@app.route('/delete_event', methods=['DELETE'])
+def delete_event():
+    conn = sqlite3.connect('events.db')
+    c = conn.cursor()
+    data = request.json
+    event_id = data['id']
+
+    c.execute("DELETE FROM events WHERE id = ?", (event_id,))
+
+    conn.commit()
+    conn.close()
+    return jsonify({"status": "success"})
 
 
 if __name__ == '__main__':
     conn = sqlite3.connect('events.db', check_same_thread=False)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS events
-                 (day integer, name text, startTime text, endTime text, description text, color text)''')
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              day integer, name text, startTime text, 
+              endTime text, description text, color text)''')
     conn.commit()
     conn.close()
     app.run(debug=True)
