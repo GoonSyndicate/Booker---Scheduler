fb889aae5ca75633c0e4c7e4abc5eed92f424851
Jmackles <124995231+Jmackles@users.noreply.github.com>
2023-09-28 20:51:26 -0400

    Full basic soyboy functionality. Holy ravioli I did a formuoli

--- eventdb.py
+++ eventdb.py
@@ -5,53 +5,56 @@ import sqlite3
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/add_event', methods=['POST'])
-def add_event():
-    conn = sqlite3.connect('events.db')
-    c = conn.cursor()
-    data = request.json
-    day = data['day']
-    name = data['name']
-    startTime = data['startTime']
-    endTime = data['endTime']
-    description = data['description']
-    color = data['color']
-    
-    c.execute("INSERT INTO events (day, name, startTime, endTime, description, color) VALUES (?, ?, ?, ?, ?, ?)",
-          (day, name, startTime, endTime, description, color))
-    
-    conn.commit()
-    conn.close()
-    return jsonify({"status": "success"})
 
 @app.route('/get_events', methods=['GET'])
 def get_events():
     conn = sqlite3.connect('events.db')
     c = conn.cursor()
-    c.execute("SELECT id, day, name, startTime, endTime, description, color FROM events")
+    c.execute("SELECT * FROM events")
     events = c.fetchall()
     conn.close()
     return jsonify(events)
+
+@app.route('/add_event', methods=['POST'])
+def add_event():
+    conn = sqlite3.connect('events.db')
+    c = conn.cursor()
+    data = request.json
+    try:
+        c.execute("INSERT INTO events (day, name, startTime, endTime, description, color) VALUES (?, ?, ?, ?, ?, ?)",
+                  (data['day'], data['name'], data['startTime'], data['endTime'], data['description'], data['color']))
+        conn.commit()
+        print("Data inserted successfully")
+        return jsonify({"status": "success"})
+    except sqlite3.Error as e:
+        print("SQL error:", e)
+        conn.rollback()
+        return jsonify({"status": "failed", "reason": str(e)})
+    finally:
+        conn.close()
+
 
 @app.route('/update_event', methods=['PUT'])
 def update_event():
     conn = sqlite3.connect('events.db')
     c = conn.cursor()
     data = request.json
-    event_id = data['id']
-    day = data['day']
-    name = data['name']
-    startTime = data['startTime']
-    endTime = data['endTime']
-    description = data['description']
-    color = data['color']
-
-    c.execute("UPDATE events SET day = ?, name = ?, startTime = ?, endTime = ?, description = ?, color = ? WHERE id = ?",
-              (day, name, startTime, endTime, description, color, event_id))
-
-    conn.commit()
-    conn.close()
-    return jsonify({"status": "success"})
+    try:
+        if data['id']:
+            c.execute("UPDATE events SET day = ?, name = ?, startTime = ?, endTime = ?, description = ?, color = ? WHERE id = ?",
+                      (data['day'], data['name'], data['startTime'], data['endTime'], data['description'], data['color'], data['id']))
+            conn.commit()
+            print("Data updated successfully")
+            return jsonify({"status": "success"})
+        else:
+            print("No id provided for update. Ignoring.")
+            return jsonify({"status": "failed", "reason": "No id provided for update"})
+    except sqlite3.Error as e:
+        print("SQL error:", e)
+        conn.rollback()
+        return jsonify({"status": "failed", "reason": str(e)})
+    finally:
+        conn.close()
 
 
 @app.route('/delete_event', methods=['DELETE'])
@@ -59,22 +62,24 @@ def delete_event():
     conn = sqlite3.connect('events.db')
     c = conn.cursor()
     data = request.json
-    event_id = data['id']
-
-    c.execute("DELETE FROM events WHERE id = ?", (event_id,))
-
-    conn.commit()
-    conn.close()
-    return jsonify({"status": "success"})
-
-
+    try:
+        c.execute("DELETE FROM events WHERE id = ?", (data['id'],))
+        conn.commit()
+        return jsonify({"status": "success"})
+    except sqlite3.Error as e:
+        print("SQL error:", e)
+        conn.rollback()
+        return jsonify({"status": "failed", "reason": str(e)})
+    finally:
+        conn.close()
+    
 if __name__ == '__main__':
-    conn = sqlite3.connect('events.db', check_same_thread=False)
+    conn = sqlite3.connect('events.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS events
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-              day integer, name text, startTime text, 
-              endTime text, description text, color text)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  day INTEGER, name TEXT, startTime TEXT, 
+                  endTime TEXT, description TEXT, color TEXT)''')
     conn.commit()
     conn.close()
     app.run(debug=True)
--- script.js
+++ script.js
@@ -1,179 +1,180 @@
 // script.js
-const events = {};  
-const today = new Date();  
-const currentYear = today.getFullYear();  
-const currentMonth = today.getMonth();  
+let events = {};
+const today = new Date();
+const currentYear = today.getFullYear();
+const currentMonth = today.getMonth();
 
 const fetchEvents = () => {
-  $.ajax({
-    url: "http://localhost:5000/get_events",
-    type: "GET",
-    success: function(response) {
-      // Clear the events object to prevent duplication
-      Object.keys(events).forEach((key) => {
-        delete events[key];
-      });
-
-      response.forEach(event => {
-        const day = event[0];
-        if (!events[day]) {
-          events[day] = [];
-        }
-        events[day].push({
-          name: event[1],
-          startTime: event[2],
-          endTime: event[3],
-          description: event[4],
-          color: event[5]
-        });
-      });
-      generateCalendar();
-    }
+  console.log("Fetching events from server...");
+  $.get("http://localhost:5000/get_events", function (response) {
+    console.log("Events fetched from server: ", response);
+    events = {}; // Clear existing events
+    response.forEach(([id, day, name, startTime, endTime, description, color]) => {
+      if (!events[day]) events[day] = [];
+      events[day].push({ id, name, startTime, endTime, description, color });
+    });
+    generateCalendar();
   });
 };
 
-// Call fetchEvents function when the page loads
-$(document).ready(function() {
-  fetchEvents();
-});
+const saveEvent = () => {
+  const form = document.getElementById("event-form");
+  const data = {
+    day: form.dataset.day,
+    name: document.getElementById("event-name").value.trim(),
+    startTime: document.getElementById("event-start-time").value,
+    endTime: document.getElementById("event-end-time").value,
+    description: document.getElementById("event-description").value.trim(),
+    color: document.getElementById("event-color").value
+  };
+
+  if (form.dataset.id && form.dataset.id !== "null") {
+    data.id = form.dataset.id;
+  } else {
+    data.id = null;
+  }
+  
+
+
+  // Field Validation
+  if (!data.name || !data.startTime || !data.endTime || !data.description || !data.color) {
+      alert("All fields are required");
+      return;
+  }
+
+  // Time Bounds Check
+  if (data.startTime > "23:59" || data.endTime > "23:59" || data.startTime < "00:00" || data.endTime < "00:00") {
+      alert("Invalid time");
+      return;
+  }
+
+  console.log("Data being sent to server: ", data);
+
+  const url = data.id ? "http://localhost:5000/update_event" : "http://localhost:5000/add_event";
+  const method = data.id ? "PUT" : "POST";
+
+  $.ajax({
+    url,
+    type: method,
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    success: function(response) {
+      console.log("Server response: ", response);
+      fetchEvents();
+    },
+    complete: discardEvent
+  });
+};
+
+
+const deleteEvent = () => {
+  const id = document.getElementById("event-form").dataset.id;
+  if (!id) return;
+  $.ajax({
+    url: "http://localhost:5000/delete_event",
+    type: "DELETE",
+    data: JSON.stringify({ id }),
+    contentType: "application/json; charset=utf-8",
+    success: fetchEvents,
+    complete: discardEvent
+  });
+};
+
+const editEvent = (day, index) => {
+  const event = events[day][index];
+  const form = document.getElementById("event-form");
+  form.dataset.id = event.id;
+  console.log("Setting form.dataset.id to ", event.id);
+  form.dataset.day = day;
+  form.dataset.index = index;
+  document.getElementById("event-name").value = event.name;
+  document.getElementById("event-start-time").value = event.startTime;
+  document.getElementById("event-end-time").value = event.endTime;
+  document.getElementById("event-color").value = event.color;
+  document.getElementById("event-description").value = event.description;
+  document.getElementById("form-title").textContent = "Edit Event";
+  form.style.display = "block";
+};
+
+const discardEvent = () => {
+  const form = document.getElementById("event-form");
+  form.dataset.id = null;
+  form.dataset.day = null;
+  form.dataset.index = null;
+  form.style.display = "none";
+};
+
+const selectDate = (day) => {
+  const form = document.getElementById("event-form");
+  form.dataset.id = null;
+  form.dataset.day = day;
+  form.dataset.index = null;
+  document.getElementById("form-title").textContent = "Add Event";
+  form.style.display = "block";
+};
 
 const generateCalendar = (year = currentYear, month = currentMonth) => {
   const tbody = document.getElementById("calendar-body");
   tbody.innerHTML = "";
+
   const firstDay = new Date(year, month, 1).getDay();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
-  
+
   for (let i = 0; i < 5; i++) {
-      const row = document.createElement("tr");
-      
-      for (let j = 0; j < 7; j++) {
-          const cell = document.createElement("td");
-          const cellWrapper = document.createElement("div");
-          cellWrapper.className = "cell-wrapper";
-          
-          const currentDay = new Date(year, month, i * 7 + j - firstDay + 1).getDate();
-          
-          // Check if the day is within the current month
-          if ((i === 0 && j < firstDay) || currentDay > daysInMonth) {
-              cellWrapper.innerHTML = "";
-          } else {
-              const dayDiv = document.createElement("div");
-              dayDiv.className = "day";
-              dayDiv.innerHTML = currentDay;
-              
-              // Append the dayDiv directly to the cell, not the cellWrapper
-              cell.appendChild(dayDiv);
-              
-              cell.onclick = () => selectDate(currentDay);
-              
-              if (events[currentDay]) {
-                  events[currentDay].forEach((event, index) => {
-                      const eventDiv = document.createElement("div");
-                      eventDiv.className = "event";
-                      eventDiv.style.backgroundColor = event.color;
-                      eventDiv.innerHTML = `${event.name} <span>${event.startTime} - ${event.endTime}</span>`;
-                      eventDiv.title = event.description;
-                      eventDiv.onclick = (e) => {
-                          e.stopPropagation();
-                          editEvent(currentDay, index);
-                      };
-                      cellWrapper.appendChild(eventDiv);
-                  });
-              }
-          }
-          
-          if (j === 0 || j === 6) {
-              cell.classList.add("weekend");
-          }
-          
-          cell.appendChild(cellWrapper);
-          row.appendChild(cell);
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement("td");
+      const cellWrapper = document.createElement("div");
+      cellWrapper.className = "cell-wrapper";
+
+      const currentDay = new Date(year, month, i * 7 + j - firstDay + 1).getDate();
+
+      if ((i === 0 && j < firstDay) || currentDay > daysInMonth) {
+        cellWrapper.innerHTML = "";
+      } else {
+        const dayDiv = document.createElement("div");
+        dayDiv.className = "day";
+        dayDiv.innerHTML = currentDay;
+
+        cellWrapper.onclick = () => selectDate(currentDay);
+
+        cell.appendChild(dayDiv);
+
+        if (events[currentDay]) {
+          events[currentDay].forEach((event, index) => {
+            const eventDiv = document.createElement("div");
+            eventDiv.className = "event";
+            eventDiv.style.backgroundColor = event.color;
+            eventDiv.innerHTML = `${event.name} <span>${event.startTime} - ${event.endTime}</span>`;
+            eventDiv.title = event.description;
+
+            eventDiv.onclick = (e) => {
+              e.stopPropagation();
+              editEvent(currentDay, index);
+            };
+
+            cellWrapper.appendChild(eventDiv);
+          });
+        }
       }
-      
-      tbody.appendChild(row);
+
+      if (j === 0 || j === 6) {
+        cell.classList.add("weekend");
+      }
+
+      cell.appendChild(cellWrapper);
+      row.appendChild(cell);
+    }
+
+    tbody.appendChild(row);
   }
-  
+
   document.querySelector("h1").textContent = `Booking Calendar for ${new Date(year, month).toLocaleString('default', { month: 'long' })} ${year}`;
 };
 
-const generateWeekView = (day) => {  
-  const weekView = document.getElementById("week-view");  
-  weekView.innerHTML = "";  
-  for (let i = 0; i < 7; i++) {  
-    const dayDiv = document.createElement("div");  
-    dayDiv.className = "day";  
-    for (let j = 0; j < 24; j++) {  
-      const hourDiv = document.createElement("div");  
-      hourDiv.className = "hour";  
-      hourDiv.dataset.hour = `${j}:00`;  
-      dayDiv.appendChild(hourDiv);  
-    }  
-    weekView.appendChild(dayDiv);  
-  }  
-  weekView.style.display = "flex";  
-};  
 
-const selectDate = (day) => {  
-  document.getElementById('form-date').innerText = `${new Date(currentYear, currentMonth, day).toLocaleString('default', { month: 'long' })} ${day}, ${currentYear}`;  
-  document.getElementById('event-form').style.display = 'block';  
-  document.getElementById('event-form').dataset.day = day;  
-  document.getElementById('event-form').dataset.index = -1;  // For new events  
-  document.getElementById('form-title').textContent = 'Add Event';
-  generateWeekView(day);  
-};
-
-const editEvent = (day, index) => {  
-  const event = events[day][index];  
-  document.getElementById('form-date').innerText = `${new Date(currentYear, currentMonth, day).toLocaleString('default', { month: 'long' })} ${day}, ${currentYear}`;  
-  document.getElementById('event-form').style.display = 'block';  
-  document.getElementById('event-form').dataset.day = day;  
-  document.getElementById('event-form').dataset.index = index;  
-  document.getElementById('event-name').value = event.name;  
-  document.getElementById('event-start-time').value = event.startTime;  
-  document.getElementById('event-end-time').value = event.endTime;  
-  document.getElementById('event-color').value = event.color;  
-  document.getElementById('event-description').value = event.description;  
-  document.getElementById('form-title').textContent = 'Edit Event';
-}; 
-
-const saveEvent = () => {
-  const day = document.getElementById('event-form').dataset.day;
-  const name = document.getElementById('event-name').value;
-  const startTime = document.getElementById('event-start-time').value;
-  const endTime = document.getElementById('event-end-time').value;
-  const description = document.getElementById('event-description').value;
-  const color = document.getElementById('event-color').value;
-
-  $.ajax({
-    url: "http://localhost:5000/add_event",
-    type: "POST",
-    dataType: "json",
-    data: JSON.stringify({
-      day,
-      name,
-      startTime,
-      endTime,
-      description,
-      color,
-    }),
-    contentType: "application/json; charset=utf-8",
-    success: function (response) {
-      fetchEvents();
-      discardEvent();
-    },
-  });
-};
-
-const discardEvent = () => {  
-  document.getElementById('event-form').style.display = 'none';  
-  document.getElementById('event-name').value = '';  
-  document.getElementById('event-start-time').value = '';  
-  document.getElementById('event-end-time').value = '';  
-  document.getElementById('event-color').value = '#4285f4';  
-  document.getElementById('event-description').value = '';  
-};  
-
-$(document).ready(function() {
+$(document).ready(() => {
   fetchEvents();
   generateCalendar();
 });
